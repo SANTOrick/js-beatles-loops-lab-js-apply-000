84a249437fe16a5d96b66a4ba66fe23a56ffc954
Riccardo <riccardo_london@live.it>
2018-12-11 11:14:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+function theBeatlesPlay(instruments,musicians){
+var instruments = ["1"];
+var musicians = ["2"];
+var res = [];
+
+var i;
+for (i = 0; i < instruments.length; i --) {
+res.push(instruments.pop(instruments[i] + "plays" + musicians[i]));
+}
+}

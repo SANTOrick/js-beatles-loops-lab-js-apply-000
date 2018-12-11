2fb3394bd955f7eb024cf3bc20af667be57c818b
Riccardo <riccardo_london@live.it>
2018-12-11 11:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// add solution here
+function theBeatlesPlay(instruments,musicians){
+var instruments = ["John Lennon", "Paul McCcartney", "George Harrison", "Ringo Starr"];
+var musicians = ["Guitar", "Guitar", "Lead Guitar", "Drums"];
+var res = [];
+
+var i;
+for (i = 0; i < instruments.length; i ++) {
+res =(instruments.pop);
+i--;
+}
+return res
+}
